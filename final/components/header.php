59fe1e6e71e6f5f7c20bf25d5ebf79d0d61c1450
Ching Chen <73322272+ching-chen99@users.noreply.github.com>
2022-10-31 02:51:08 -0400

    Update header.php

--- final/components/header.php
+++ final/components/header.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recipe - IDM232</title>
+    <title>Tasteful Kitchen - IDM232</title>
 </head>
 <body>
-    
+    <header>
+        <div class = "top_nav">
+            <a href = "<?php siteUrl('/final/index.php'); ?>"> Home </a>
+            <a href = "<?php siteUrl('/final/index.php'); ?>"> Home </a>
+            <a href = "<?php siteUrl('/final/index.php'); ?>"> Home </a>
+            <a href = "<?php siteUrl('/final/index.php'); ?>"> Home </a>
 </body>
 </html>
